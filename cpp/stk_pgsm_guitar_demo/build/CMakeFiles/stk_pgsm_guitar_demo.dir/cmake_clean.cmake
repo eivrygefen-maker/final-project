cace28cbecc265f23fa544b7ddd38d2e663e5f6b
eivry gefen <eivrygefen@mail.tau.ac.il>
2026-06-15 19:41:12 +0300

    Refactor code structure for improved readability and maintainability

---
+++ cpp/stk_pgsm_guitar_demo/build/CMakeFiles/stk_pgsm_guitar_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stk_pgsm_guitar_demo.dir/main.cpp.o"
+  "CMakeFiles/stk_pgsm_guitar_demo.dir/main.cpp.o.d"
+  "stk_pgsm_guitar_demo"
+  "stk_pgsm_guitar_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stk_pgsm_guitar_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/stk_pgsm_guitar_demo/build/CMakeFiles/stk_pgsm_guitar_demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stk_pgsm_guitar_demo.
+# This may be replaced when dependencies are built.
